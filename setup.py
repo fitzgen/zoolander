e982c3d2297d39d108b31f3f41d66449b700dc1f
Nick Fitzgerald <fitzgen@gmail.com>
2010-04-27 21:13:39 -0700

    Adding a setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="zoolander",
+      version="0.1",
+      description="A pure Python DSL for CSS",
+      author="Nick Fitzgerald",
+      author_email="fitzgen@gmail.com",
+      url="http://github.com/fitzgen/zoolander",
+      packages=["zoolander"],
+      license="MIT")
