3918220d5cee183c408753a515a8f1baa3244999
Nick Fitzgerald <fitzgen@gmail.com>
2010-04-28 15:32:33 -0700

    Updating the example_styles.css to use the new units

--- example_styles.py
+++ example_styles.py
@@ -11,26 +11,29 @@ with sheet.definitions as rule:
          line_height=2)
 
     rule("h1",
-         font_size="3em",
-         font_weight="bold")
+         font_size=em(3),
+         font_weight=bold)
 
     # Since this is pure Python code, we can use all of Python's control
-    # structures, simplest of which are variables.
+    # structures, simplest of which are variables and arithmetic operators.
 
-    SIDE_PANEL_BG_COLOR = "#AACCCC"
+    side_panel_bg_color = "#AACCCC"
+    spacing = em(2)
 
     rule("#side-panel",
-         background_color=SIDE_PANEL_BG_COLOR)
+         margin=spacing/2,
+         padding=spacing+1,
+         background_color=side_panel_bg_color)
 
     # Use dictionaries to mix in sets of properties to multiple rules.
 
     toolbar_mixin = dict(background_color="#556677",
                          float="left",
-                         margin="1em")
+                         margin=em(1))
 
     rule("#toolbar li",
          color="#005599",
-         font_style="italic",
+         font_style=italic,
          **toolbar_mixin)
 
     rule("#toolbar img",
